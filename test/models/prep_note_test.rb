eaa9bdbdf5690abf9428c365aad053040bb70436
Tanner Wilson <tanner28aw@gmail.com>
2017-11-20 21:21:57 +0000

    Added annotations to new tables

--- test/models/prep_note_test.rb
+++ test/models/prep_note_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: prep_notes
+#
+#  id         :integer          not null, primary key
+#  note       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class PrepNoteTest < ActiveSupport::TestCase
--- test/models/unit_test.rb
+++ test/models/unit_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: units
+#
+#  id         :integer          not null, primary key
+#  unit       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class UnitTest < ActiveSupport::TestCase
