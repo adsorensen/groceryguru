cb141c2bcc3cde622ca18f241ef61129b8046efc
Rob Olsen <rolsenrob@gmail.com>
2017-11-26 22:53:48 +0000

    Added cart DB table for storing cart info

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120200553) do
+ActiveRecord::Schema.define(version: 20171126223206) do
+
+  create_table "carts", force: :cascade do |t|
+    t.integer  "user",       limit: 4
+    t.integer  "recipe",     limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "ingredient", limit: 255
