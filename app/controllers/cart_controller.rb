ea4d3148f3e3069c8fa240f6fa1791055eef2700
Rob Olsen <rolsenrob@gmail.com>
2017-11-27 20:42:59 +0000

    updated so recipes display again

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -56,7 +56,7 @@ class CartController < ApplicationController
   def destroy
     @cart.destroy
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to '/cart', notice: 'Cart was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
