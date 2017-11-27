b645008608d34d774a321dbbee4dd4a2b1c1cf4b
Rob Olsen <rolsenrob@gmail.com>
2017-11-27 20:07:43 +0000

    removed extra migration file

--- db/migrate/20171122004421_drop_tables.rb
+++
@@ -1,9 +0,0 @@
-class DropTables < ActiveRecord::Migration
-  def change
-    drop_table :recipes
-    drop_table :units
-    drop_table :prep_notes
-    drop_table :ingredients
-    drop_table :instructions
-  end
-end
