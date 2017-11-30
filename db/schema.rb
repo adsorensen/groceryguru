00636488f92f7169e98b207523d160a29faf923b
Michelle Nguyen <michellenguy3n@hotmail.com>
2017-11-30 19:15:35 +0000

    Recipes picture uploading

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127224151) do
+ActiveRecord::Schema.define(version: 20171130185113) do
 
   create_table "carts", force: :cascade do |t|
     t.integer  "user",       limit: 4
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20171127224151) do
     t.string   "origin",      limit: 255
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.string   "picture",     limit: 255
   end
 
   create_table "saved_recipes", force: :cascade do |t|
