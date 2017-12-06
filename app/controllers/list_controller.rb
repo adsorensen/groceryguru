0c6f495a6cb5ffbd6395607e1cc9eda50dbedaae
Adam Sorensen <adam.sorensen455@gmail.com>
2017-12-06 22:17:02 +0000

    Can create ingredient list for shopping, need to get check boxes working.

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -9,15 +9,17 @@ class ListController < ApplicationController
     recipe_ids = Cart.where(user: session['user_id']).order(created_at: :desc)
     #render 'list/create', :locals => {:resource => recipe_ids}
     @ingredients = []
-    st = "blah"
+    st = ""
     for id_val in recipe_ids do
       recipe = Recipe.where(id: id_val.recipe).first
       instr = recipe.instructions
       for i in instr do
-        st += Ingredient.find(i.ingredient_id).name.to_s
+        @ingredients.append(Ingredient.find(i.ingredient_id).name.to_s)
       end
-      #ing = 
+       
     end
-    render :text => st
+    @ingredients.uniq
+    render 'list/create', :locals => {:resource => @ingredients}
+    #render :text => st
   end
 end
