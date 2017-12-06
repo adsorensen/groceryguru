b441bcba4dde6932dd628424d8869ce62f56481a
Adam Sorensen <assassinadam@hotmail.com>
2017-12-06 02:08:04 +0000

    schema.rb edited online with Bitbucket

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206014930) do
+ActiveRecord::Schema.define(version: 20171203030159) do
 
   create_table "carts", force: :cascade do |t|
     t.integer  "user",       limit: 4
