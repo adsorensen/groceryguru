6ac0af2118c7890fcb01046685d0d9b4265d4c52
Michelle Nguyen <michellenguy3n@hotmail.com>
2017-12-08 05:47:40 +0000

    Removed row from calendar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206190406) do
+ActiveRecord::Schema.define(version: 20171208021107) do
 
   create_table "carts", force: :cascade do |t|
     t.integer  "user",       limit: 4
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 20171206190406) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.string   "picture",     limit: 255
+    t.integer  "servings",    limit: 4
+    t.integer  "prep_time",   limit: 4
   end
 
   create_table "reviews", force: :cascade do |t|
