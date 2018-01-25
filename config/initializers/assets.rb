334e9a82379df2ae707a567bd826eee792aaf9f1
Adam Sorensen <adam.sorensen455@gmail.com>
2018-01-25 23:56:23 +0000

    commented out lines in config/init/assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,19 +2,19 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( styles.css )
-Rails.application.config.assets.precompile += %w( welcome.css )
-Rails.application.config.assets.precompile += %w( recipes.css )
-Rails.application.config.assets.precompile += %w( recipes_tab.css )
-Rails.application.config.assets.precompile += %w( new_recipes.css )
-Rails.application.config.assets.precompile += %w( cart.css )
-Rails.application.config.assets.precompile += %w( users.css )
-Rails.application.config.assets.precompile += %w( users_new.css )
-Rails.application.config.assets.precompile += %w( users_show.css )
-Rails.application.config.assets.precompile += %w( users_edit.css )
-Rails.application.config.assets.precompile += %w( list.css )
-Rails.application.config.assets.precompile += %w( calendar.css )
-Rails.application.config.assets.precompile += %w( newRecipe.js )
+# Rails.application.config.assets.precompile += %w( styles.css )
+# Rails.application.config.assets.precompile += %w( welcome.css )
+# Rails.application.config.assets.precompile += %w( recipes.css )
+# Rails.application.config.assets.precompile += %w( recipes_tab.css )
+# Rails.application.config.assets.precompile += %w( new_recipes.css )
+# Rails.application.config.assets.precompile += %w( cart.css )
+# Rails.application.config.assets.precompile += %w( users.css )
+# Rails.application.config.assets.precompile += %w( users_new.css )
+# Rails.application.config.assets.precompile += %w( users_show.css )
+# Rails.application.config.assets.precompile += %w( users_edit.css )
+# Rails.application.config.assets.precompile += %w( list.css )
+# Rails.application.config.assets.precompile += %w( calendar.css )
+# Rails.application.config.assets.precompile += %w( newRecipe.js )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
