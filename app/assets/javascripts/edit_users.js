e20d69106c835efc1a91b15edd16668d7c4bc47f
Michelle Nguyen <michellenguy3n@hotmail.com>
2018-02-13 06:58:54 +0000

    Removed diff files and unneccesary js function

--- app/assets/javascripts/edit_users.js
+++ app/assets/javascripts/edit_users.js
@@ -9,24 +9,3 @@ $('#notification_toggle').on('change', function() {
     $('.judgments-settings').addClass('disabled').stop().animate({ height: 0 }, 500);;
   }
 });
-
-function updateFullName()
-{
-   document.getElementById('fullname').style.display = "none"
-}
-
-function updateUsername(username)
-{
-  if (document.getElementById('username_button').textContent == 'Click here to update')
-  {
-   document.getElementById('username').style.display = "none"
-   document.getElementById('username_textbox').style.display = "inline-block"
-   document.getElementById('username_button').textContent = 'Cancel'
-  }
-  else if (document.getElementById('username_button').textContent == 'Cancel')
-  {
-   document.getElementById('username').style.display = "inline-block"
-   document.getElementById('username_textbox').style.display = "none"
-   document.getElementById('username_button').textContent = "Click here to update"
-  }
-}
