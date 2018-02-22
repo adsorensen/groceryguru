f00b6322f3e7975a2a8247d315940aae0035a4d7
Adam Sorensen <adam.sorensen455@gmail.com>
2018-02-22 06:00:15 +0000

    Changed amounts to input, made dictionary of units and values, added item input field (need to finish).

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -7,35 +7,56 @@ class ListController < ApplicationController
   
   # function converts differnent units to ounces
   def convert_to_oz(amount, unit)
-    u = unit.upcase
+    conversions = {"TABLESPOON" => 0.5, "TBSP" => 0.5, "TEASPOON" => 0.16667, 
+      "TSP" => 0.16667, "CUP" => 8, "HANDFUL" => 4, "OUNCE" => 1, "PINCH" =>
+      0.013, "SERVING" => 4, "POUND" => 16, "LBS" => 16, "CLOVE" => 0.16667,
+      "HEAD" => 32, "OZ" => 1, "C" => 8, "BUNCH" => 0, "PIECE" => 0, "G" =>
+      0, "TBS" => 0.5, "SLICES" => 0, "CAN" => 0, "DASHES" => 0,
+      "SPRIGS" => 0, "PINCHES" => 0, "DASH" => 0, "PINT" => 0, "TB" => 0.5,
+      "KG" => 0, "SPRIG" => 0, "ML" => 0, "" => 1
+    }
     noUnit = false
-    case u
-    when "TABLESPOON", "TABLESPOONS", "TBSP"
-      newA = amount / 2
-    when "TEASPOON", "TEASPOONS", "TSP"
-      newA = amount * (1/6)
-    when "CUP", "CUPS", "C"
-      newA = amount * 8
-    when "HANDFUL", "HANDFULS"
-      newA = amount * 4
-    when "OZ", "OUNCE", "OUNCES"
-      newA = amount
-    when "LBS", "POUND", "POUNDS"
-      newA = amount * 16
-    when "PINCH", "PINCHES"
-      newA = amount * 0.013
-    when "SERVINGS", "SERVING"
-      newA = amount * 4
-    when ""
-      newA = amount
-      noUnit = true
-    when "CLOVE", "CLOVES"
-      newA = amount * (1/6)
-    when "HEAD", "HEADS"
-      newA = amount * 32
+    if conversions.include? unit.upcase
+      if unit == ""
+        noUnit = true
+      end
+      v = conversions[unit.upcase]
+      newA = amount * v
+    elsif conversions.include? unit.upcase[0..-2]
+      v = conversions[unit.upcase[0..-2]]
+      newA = amount * v
     else
       newA = -100
     end
+    
+    # u = unit.upcase
+    # case u
+    # when "TABLESPOON", "TABLESPOONS", "TBSP"
+    #   newA = amount / 2
+    # when "TEASPOON", "TEASPOONS", "TSP"
+    #   newA = amount * 0.16667
+    # when "CUP", "CUPS", "C"
+    #   newA = amount * 8
+    # when "HANDFUL", "HANDFULS"
+    #   newA = amount * 4
+    # when "OZ", "OUNCE", "OUNCES"
+    #   newA = amount
+    # when "LBS", "POUND", "POUNDS"
+    #   newA = amount * 16
+    # when "PINCH", "PINCHES"
+    #   newA = amount * 0.013
+    # when "SERVINGS", "SERVING"
+    #   newA = amount * 4
+    # when ""
+    #   newA = amount
+    #   noUnit = true
+    # when "CLOVE", "CLOVES"
+    #   newA = amount * 0.16667
+    # when "HEAD", "HEADS"
+    #   newA = amount * 32
+    # else
+    #   newA = -100
+    # end
     
     return newA, noUnit
   end
