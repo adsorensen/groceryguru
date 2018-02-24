4589a94fbb0d13034009477b6fd936c1710bb35c
Rob Olsen <rolsenrob@gmail.com>
2018-02-24 20:41:49 +0000

    added print statement for debug

--- store_integration/server/job_done.py
+++ store_integration/server/job_done.py
@@ -7,4 +7,5 @@ def job_done(userId):
     # hash = ""
     payload = {"userId": userId}
     print("Sending job complete: {}".format(str(userId)))
-    response = requests.post('https://capstone-cs-4000-grocery-guru-rolsenrob.c9users.io', data=payload)
+    response = requests.post('https://capstone-cs-4000-grocery-guru-rolsenrob.c9users.io/products/done', data=payload)
+    print(response)
