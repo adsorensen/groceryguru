c9fd032bd5e13b4837ed69001034305be543f283
Tanner Wilson <tanner28aw@gmail.com>
2018-02-24 20:41:39 +0000

    done stuffs

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
-  before_action :set_user, only: [:done]
+  # before_action :set_user, only: [:done]
   def done
+    puts("Received Done")
   end
   
   private 
