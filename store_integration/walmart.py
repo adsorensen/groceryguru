47eed037d10b1547f0d3fd24ad70708abbc98d1c
Michelle Nguyen <michellenguy3n@hotmail.com>
2018-03-08 06:21:49 +0000

    Aesthetic cart changes

--- store_integration/walmart.py
+++ store_integration/walmart.py
@@ -107,7 +107,7 @@ class Walmart(Store):
     #*****************************************************************
     def run_job(self, user_id):
         # Connect to DB, grab checklist products 
-        self.login('', '')
+        self.login('robolsen92@gmail.com', 'FurOtpKw49gR')
         db = Database()
         ingredients = db.get_checklist_ingredients(user_id)
         print(ingredients)
