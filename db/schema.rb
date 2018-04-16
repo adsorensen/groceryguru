c7dfd53b82a3c93d59811abda7b427428add664b
Rob Olsen <rolsenrob@gmail.com>
2018-04-16 00:45:03 +0000

    Updated iframe to work for smiths and made it look better. also fixed a couple minor bugs with list stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180330003056) do
+ActiveRecord::Schema.define(version: 20180405193313) do
 
   create_table "carts", force: :cascade do |t|
     t.integer  "user",       limit: 4
