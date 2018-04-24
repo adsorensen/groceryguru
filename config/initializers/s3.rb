6f8873ff4a91d138673964cb6c0fb346e09622d4
Michelle Nguyen <michellenguy3n@hotmail.com>
2018-04-24 22:43:29 +0000

    Trying to get pictures working on Heroku

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => "AKIAIMCGFYMMZPA73PKQ",
-      :aws_secret_access_key  => "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",
+      :aws_access_key_id      => "AKIAIULLUHQ2KLCVIZAQ",
+      :aws_secret_access_key  => "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",
       :region                 => 'us-east-1' # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = "aws-website-groceryguru-34jq0"
