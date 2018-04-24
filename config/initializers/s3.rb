f14e12eb259c78fea7c9ef64a65c030f90fd7ade
Michelle Nguyen <michellenguy3n@hotmail.com>
2018-04-24 22:49:50 +0000

    New keys

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => "AKIAIULLUHQ2KLCVIZAQ",
-      :aws_secret_access_key  => "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",
+      :aws_access_key_id      => "AKIAJSOZALNLNHJKMHZQ",
+      :aws_secret_access_key  => "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",
       :region                 => 'us-east-1' # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = "aws-website-groceryguru-34jq0"
