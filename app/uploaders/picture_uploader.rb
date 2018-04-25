3739e5a132affd3d38e189ebc8ba416257e2e50a
Michelle Nguyen <michellenguy3n@hotmail.com>
2018-04-25 06:02:17 +0000

    It's impossible

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,7 +7,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   process resize_to_fill: [150, 150]
 
   # Choose what kind of storage to use for this uploader:
-  include CarrierWaveDirect::Uploader
+  storage :file
   
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,3 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAIQRMHKZ7A25QM5SA',
-    :aws_secret_access_key  => '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'
-  }
-  config.fog_directory  = 'aws-website-groceryguru-34jq0' # bucket name
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
 end
