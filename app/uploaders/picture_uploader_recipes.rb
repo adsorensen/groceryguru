4ed5f01bb7c9108c8e38227b829a3c7397f9f068
Michelle Nguyen <michellenguy3n@hotmail.com>
2018-04-25 05:59:43 +0000

    Sick of dis shit

--- app/uploaders/picture_uploader_recipes.rb
+++ app/uploaders/picture_uploader_recipes.rb
@@ -7,7 +7,7 @@ class PictureUploaderRecipes < CarrierWave::Uploader::Base
   process resize_to_fill: [500, 300]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  include CarrierWaveDirect::Uploader
   #storage :fog
 
   # Override the directory where uploaded files will be stored.
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,8 @@
 CarrierWave.configure do |config|
-  config.cache_dir = "#{Rails.root}/tmp/uploads"
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => 'AKIAIQRMHKZ7A25QM5SA',
+    :aws_secret_access_key  => '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'
+  }
+  config.fog_directory  = 'aws-website-groceryguru-34jq0' # bucket name
 end
