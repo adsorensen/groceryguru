12d5a69801be1a81dcbc3908d08c9c68247bac25
Michelle Nguyen <michellenguy3n@hotmail.com>
2018-04-25 05:19:22 +0000

    Trying pictures one last time

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,10 @@
 CarrierWave.configure do |config|
-  config.cache_dir = "#{Rails.root}/tmp/uploads"
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     'AKIAIL5TIRWBJWXM4TTQ',
+      aws_secret_access_key: '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',
+      region:                'us-east-1',
+  }
+  config.fog_directory  = 'aws-website-groceryguru-34jq0'
 end
